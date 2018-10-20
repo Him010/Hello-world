f53bf698807caeefd6091ea84d37914769d9b2f7
Himanshu <aggarwalhamu010@gmail.com>
2018-10-20 18:18:43 +0530

    Create hello.cpp
    to print hello world in centre of screen

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+char ch[]="HELLO WORLD!";
+gotoxy(683,384);//to print hello world in centre of screen
+cout<<ch;
+return 0;
+}
